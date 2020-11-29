a5e579e13f25cc63894205969da591bd42346614
pharrukh <farrukh.normuradov@gmail.com>
2020-11-29 20:09:00 +0100

    feat: add project 3

--- Project_3/server.js
+++ Project_3/server.js
@@ -1,19 +1,38 @@
-// Setup empty JS object to act as endpoint for all routes
-projectData = {};
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 
-// Require Express to run server and routes
+const app = express();
+const port = 3000;
+let weatherData = null;
 
-// Start up an instance of app
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+app.use(cors());
 
 // Initialize the main project folder
 app.use(express.static('website'));
 
+app.get('/weather-forecast', (req, res) => {
+    res.send(weatherData);
+});
+
+app.post('/weather-forecast', (req, res) => {
+    const weatherForecast = {
+        temperature: req.body.temperature,
+        date: req.body.date,
+        'user-response': req.body['user-response']
+    };
+    weatherData = weatherForecast;
+    res.send({ statusCode: 204 })
+});
 
 // Setup Server
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+});
