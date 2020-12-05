d978a434e49a282c85e5502d37b167790bf28edf
pharrukh <farrukh.normuradov@gmail.com>
2020-12-05 14:30:49 +0100

    chore: initial final project setup

---
+++ projects/capstone/README.md
@@ -0,0 +1,3 @@
+# Travel App
+
+## Overview
---
+++ projects/capstone/src/server/server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = 3000;
+let projectData = null;
+
+
+/* Middleware*/
+//Here we are configuring express to use body-parser as middle-ware.
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// Cors for cross origin allowance
+app.use(cors());
+
+// Initialize the main project folder
+app.use(express.static('website'));
+
+app.get('/weather-forecast', (req, res) => {
+    res.send(projectData);
+});
+
+app.post('/weather-forecast', (req, res) => {
+    const weatherForecast = {
+        temperature: req.body.temperature,
+        date: req.body.date,
+        'user-response': req.body['user-response']
+    };
+    projectData = weatherForecast;
+    res.send({ statusCode: 204 })
+});
+
+// Setup Server
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+});
